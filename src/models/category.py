56600bbf49db230c9ec60bb306fdec2cc94ea357
Escariao <42503603+escariao@users.noreply.github.com>
2025-05-24 13:01:10 -0300

    Update category.py

--- src/models/category.py
+++ src/models/category.py
@@ -1,11 +1,13 @@
-from flask_sqlalchemy import SQLAlchemy
 from src.models.user import db
 
-class ProfessionalActivity(db.Model):
-    __tablename__ = 'professional_activities'
+class Category(db.Model):
+    __tablename__ = 'categories'
+    
     id = db.Column(db.Integer, primary_key=True)
-    professional_id = db.Column(db.Integer, db.ForeignKey('professionals.id'), nullable=False)
-    activity_name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(255))
-    price = db.Column(db.Float)
-    availability = db.Column(db.String(255))
+    name = db.Column(db.String(100), nullable=False, unique=True)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
