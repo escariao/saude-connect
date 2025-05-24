bd26883283a37777c37ba0d5472fd262277047fd
Escariao <42503603+escariao@users.noreply.github.com>
2025-05-24 12:57:18 -0300

    Update category.py

--- src/models/category.py
+++ src/models/category.py
@@ -1,8 +1,11 @@
-
 from flask_sqlalchemy import SQLAlchemy
 from src.models.user import db
 
 class ProfessionalActivity(db.Model):
     __tablename__ = 'professional_activities'
     id = db.Column(db.Integer, primary_key=True)
+    professional_id = db.Column(db.Integer, db.ForeignKey('professionals.id'), nullable=False)
     activity_name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255))
+    price = db.Column(db.Float)
+    availability = db.Column(db.String(255))
