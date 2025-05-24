d09cbecb7a2384d3e881472c989daed9bfe94e73
Escariao <42503603+escariao@users.noreply.github.com>
2025-05-24 18:06:40 -0300

    Update professional_activity.py

--- src/routes/professional_activity.py
+++ src/routes/professional_activity.py
@@ -1,70 +1,30 @@
+# src/routes/professional_activity.py
+
 from flask import Blueprint, request, jsonify
-from src.models.user import db
-from src.models.professional_activity import ProfessionalActivity
+from src.models.professional_activity import ProfessionalActivity, db
 
-activity_bp = Blueprint('activity', __name__)
+professional_activity_bp = Blueprint('professional_activity', __name__, url_prefix='/api/professional_activity')
 
-@activity_bp.route('/', methods=['POST'])
-def create_activity():
-    try:
-        data = request.get_json()
-        required_fields = ['professional_id', 'activity_name']
-        for field in required_fields:
-            if field not in data:
-                return jsonify({'error': f'{field} is required'}), 400
-
-        activity = ProfessionalActivity(
-            professional_id=data['professional_id'],
-            activity_name=data['activity_name'],
-            description=data.get('description'),
-            price=data.get('price'),
-            availability=data.get('availability')
-        )
-        db.session.add(activity)
-        db.session.commit()
-        return jsonify({'message': 'Activity created', 'id': activity.id}), 201
-    except Exception as e:
-        db.session.rollback()
-        return jsonify({'error': str(e)}), 500
-
-@activity_bp.route('/<int:id>', methods=['GET'])
-def get_activity(id):
-    activity = ProfessionalActivity.query.get(id)
-    if not activity:
-        return jsonify({'error': 'Activity not found'}), 404
-    return jsonify(activity.to_dict()), 200
-
-@activity_bp.route('/', methods=['GET'])
-def list_activities():
+@professional_activity_bp.route('/', methods=['GET'])
+def list_professional_activities():
     activities = ProfessionalActivity.query.all()
-    return jsonify([a.to_dict() for a in activities]), 200
+    return jsonify([a.serialize() for a in activities]), 200
 
-@activity_bp.route('/<int:id>', methods=['PATCH'])
-def update_activity(id):
-    try:
-        activity = ProfessionalActivity.query.get(id)
-        if not activity:
-            return jsonify({'error': 'Activity not found'}), 404
+@professional_activity_bp.route('/<int:id>', methods=['GET'])
+def get_professional_activity(id):
+    activity = ProfessionalActivity.query.get_or_404(id)
+    return jsonify(activity.serialize()), 200
 
-        data = request.get_json()
-        for key in ['activity_name', 'description', 'price', 'availability']:
-            if key in data:
-                setattr(activity, key, data[key])
-        db.session.commit()
-        return jsonify({'message': 'Activity updated'}), 200
-    except Exception as e:
-        db.session.rollback()
-        return jsonify({'error': str(e)}), 500
-
-@activity_bp.route('/<int:id>', methods=['DELETE'])
-def delete_activity(id):
-    try:
-        activity = ProfessionalActivity.query.get(id)
-        if not activity:
-            return jsonify({'error': 'Activity not found'}), 404
-        db.session.delete(activity)
-        db.session.commit()
-        return jsonify({'message': 'Activity deleted'}), 200
-    except Exception as e:
-        db.session.rollback()
-        return jsonify({'error': str(e)}), 500
+@professional_activity_bp.route('/', methods=['POST'])
+def create_professional_activity():
+    data = request.json
+    new_activity = ProfessionalActivity(
+        professional_id=data['professional_id'],
+        name=data['name'],
+        description=data.get('description', ''),
+        price=data['price'],
+        years_of_experience=data.get('years_of_experience', 0)
+    )
+    db.session.add(new_activity)
+    db.session.commit()
+    return jsonify(new_activity.serialize()), 201
